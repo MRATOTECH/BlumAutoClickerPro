d58c27c0fef0ff02dada5cb1b7b77b1d36bd8632
MRATOTECH <atogondolf@gmail.com>
2024-08-28 22:41:16 +0000

    Create static.py

---
+++ main/static.py
@@ -0,0 +1,16 @@
+AUTOCLICKER_TEXT = """
+
+██████╗░██╗░░░░░██╗░░░██╗███╗░░░███╗  ░█████╗░██╗░░░██╗████████╗░█████╗░
+██╔══██╗██║░░░░░██║░░░██║████╗░████║  ██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗
+██████╦╝██║░░░░░██║░░░██║██╔████╔██║  ███████║██║░░░██║░░░██║░░░██║░░██║
+██╔══██╗██║░░░░░██║░░░██║██║╚██╔╝██║  ██╔══██║██║░░░██║░░░██║░░░██║░░██║
+██████╦╝███████╗╚██████╔╝██║░╚═╝░██║  ██║░░██║╚██████╔╝░░░██║░░░╚█████╔╝
+╚═════╝░╚══════╝░╚═════╝░╚═╝░░░░░╚═╝  ╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░
+"""
+
+CREDITS = "\033[34mOriginal: https://mratotech/BlumAutoClickerPro\033[0m"
+DONATE_TEXT = "\033[1;33;48mDonate here (TON):\033[1;37;0m UQAicWDehIx-7AI2if8WgAXSyWRY3eU_W287ZjNcnKZWE3DA" \
+              "\n\033[1;33;48mDonate here (BTC):\033[1;37;0m 1HcP7FztEnakTsafFe9i5Vzj9aCqT4cWBz\n"
+              "\n\033[1;33;48mDonate here (ETH):\033[1;37;0m 0xcfc1ba71e233d6222d8a13166da474d849cc9fe1\n"
+              "\n\033[1;33;48mDonate here (Paypal):\033[1;37;0m https://www.paypal.com/paypalme/mratotech\n"
+WINDOW_NOT_FOUND = "Blum window not found! Please open it, and restart the program."
